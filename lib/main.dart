d0141b3102687d779ccceef4677cd7c770cbdd56
reza483t <reza.hejazi.555@gmail.com>
2024-04-22 19:04:11 +0330

    final commit attribute : shadow onexit onhover ontap

--- lib/main.dart
+++ lib/main.dart
@@ -28,59 +28,80 @@ class MyHomePage extends StatefulWidget {
 }
 class _MyHomePageState extends State<MyHomePage> {
   bool hoverclick =false;
-  Color textFieldColor=Colors.black;
+  Color textFieldColor=Colors.white;
+  Color boxShadowcolor=Colors.blueGrey;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child:MouseRegion(
-  cursor: SystemMouseCursors.click,
-  child: TextField(
-    decoration: InputDecoration(
-      filled: true,
-      fillColor: textFieldColor,
-      hintText: 'Enter your text',
-    ),
-    style: TextStyle(
-      color: Colors.blueGrey,
-    ),
-    onChanged: (value) {
-    },
-    onTap: () {
-          setState(() {
-        textFieldColor = Colors.redAccent.shade400;
-      });
-    },
-    onSubmitted: (value) {
-      
-    },
-  ),
-  onHover: (hover) {
-    hoverclick=true;
-    if (hoverclick==true) {
-      setState(() {
-        textFieldColor = Colors.grey.shade400;
-      });
-    } else {
-      setState(() {
-        textFieldColor = Colors.white;
-      });
-    }
-  },
-  onExit: (event) {
-    setState(() {
-        textFieldColor = Colors.black;
-      });
-  },
-)
-
+      body: Container(
+        color: Colors.blueGrey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child:MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: Center(
+            child: Container(
+        width: 350,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: boxShadowcolor,
+              spreadRadius: 1,
+              blurRadius: 20,
+              offset: Offset(0, 1)
             )
-          ],
+          ]
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20)
+            ),
+            filled: true,
+            fillColor: textFieldColor,
+            hintText: 'Enter your text',
+          ),
+          
+          style: TextStyle(
+            color: Colors.black,
+          ),
+          
+          onChanged: (value) {
+          },
+          onTap: () {
+                setState(() {
+              textFieldColor = Colors.grey.shade500;
+            boxShadowcolor=Colors.green;
+            });
+          },
+          onSubmitted: (value) {
+            
+          },
+        ),
+            ),
+          ),
+          onHover: (hover) {
+            hoverclick=true;
+            if (hoverclick==true) {
+        setState(() {
+          textFieldColor = Colors.grey.shade400;
+        });
+            }
+          },
+          onExit: (event) {
+            setState(() {
+          textFieldColor = Colors.white;
+        });
+          },
+        )
+        
+              )
+            ],
+          ),
         ),
       ),
     );
